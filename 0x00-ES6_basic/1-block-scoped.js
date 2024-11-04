7c8bfd3b23aaf8442daf860104233e9a135f2b60
Annytech001 <godlybassey101@gmail.com>
2024-11-04 21:49:38 +0100

    blocked scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;  // Block-scoped variable
-    let task2 = false; // Block-scoped variable
+    return [task, task2];
   }
-
   return [task, task2];
 }
-
